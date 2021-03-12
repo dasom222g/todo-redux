2e38d70254996755db864b7f2d82cf9fece31aa7
dasom <50973005+dasom222g@users.noreply.github.com>
2021-03-12 18:32:46 +0900

    combine reducer

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
 import counter from './counter'
+import posts from './posts'
 
-export const rootReducer = combineReducers({ counter })
+export const rootReducer = combineReducers({ counter, posts })
